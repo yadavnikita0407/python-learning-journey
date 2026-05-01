739f9a592b1f12083f59bdb06d9f8f350c585f82
yadavnikita0407 <yadavnikitaofficial004@gmail.com>
2026-05-01 15:06:40 +0530

    Add files via upload

---
+++ variables_datatypes.py
@@ -0,0 +1,24 @@
+# variables and data types in python
+# Integer
+age=22
+print("age is :",age)
+print("type of age is :",type(age))
+
+#float
+height=4.5
+print("height is :",age)
+print("type of height is :",type(height))
+
+#string
+name="Nikita"
+print("Name is :",age)
+print("type of name is :",type(name))
+
+#boolean
+student= True
+print("student is :",age)
+print("type of student is :",type(student))
+
+#taking input from user
+your_name=input(" Enter your name :")
+print("Hi,",your_name)
